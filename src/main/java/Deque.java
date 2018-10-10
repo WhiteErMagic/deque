b105e82347e041b2b474d15273141632bf231262
WhiteErMagic <kabv@mail.ru>
2018-10-11 01:54:42 +0600

    Доработал

--- src/main/java/Deque.java
+++ src/main/java/Deque.java
@@ -1,42 +1,42 @@
 class Deque {
 
-     int size;
-     int head;
-     int tail;
-     int[] data;
+    int size;
+    int head;
+    int tail;
+    int[] data;
 
-     Deque(int size) {
-         data = new int [this.size = size];
-         head = size - 1;
-     }
+    Deque(int size) {
+        data = new int [this.size = size];
+        head = size - 1;
+    }
 
-     void pushBack(int value) {
-         data[tail++] = value;
-         if (tail == size)
+    void pushBack(int value) {
+        data[tail++] = value;
+        if (tail == size)
             tail = 0;
-     }
+    }
 
-     int popBack() {
-         if (--tail < 0)
-             tail = size - 1;
-         int ret = data[tail];
-         return ret;
-     }
+    int popBack() {
+        if (--tail < 0)
+            tail = size - 1;
+        int ret = data[tail];
+        return ret;
+    }
 
-     void pushFront(int value) {
-         data[head--] = value;
-         if (head < 0)
-             head = size - 1;
-     }
+    void pushFront(int value) {
+        data[head--] = value;
+        if (head < 0)
+            head = size - 1;
+    }
 
-     int popFront() {
-         if (++head == size)
+    int popFront() {
+        if (++head == size)
             head = 0;
-         return data[head];
-     }
+        return data[head];
+    }
 
-     boolean isEmpty() {
-         return head == tail;
-     }
+    boolean isEmpty() {
+        return head == tail;
+    }
 
 }
